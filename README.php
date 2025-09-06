0a5e7de011903df086439aa8165582875bb6a6e8
TENYWA NICHOLAS <tenywanicholas78@gmail.com>
2025-09-06 03:23:32 +0300

    comment in php

--- README.php
+++ README.php
@@ -4,6 +4,11 @@
 
         <?php
         echo "This is my first PHP script";
+        //this is a single-line comment
+        #this is also a single -line comment
+
+        /*this is a multi-line comment*/
+        // we always comment to avoid codes from being excuted
         ?>
         </body>
     </html>
